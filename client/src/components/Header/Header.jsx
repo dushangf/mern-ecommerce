8ba5ca88f84960dd65eeb8cd6632f924b2fcd463
dushangf <dushangf@gmail.com>
2022-02-19 01:54:20 +0530

    header updated

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({ cart }) => {
    return (
       <div className="flex px-20 justify-between py-6 bg-cyan-900">
          <Link to="/">
-            <h1 className="text-2xl text-emerald-300 font-semibold">E-Mart</h1>
+            <h1 className="text-2xl text-emerald-300 font-semibold">e-mart</h1>
          </Link>
          <div className="flex items-center">
             <div className="text-emerald-300 text-2xl mr-10 count-div p-2 hover:scale-125 transtion duration-150">
